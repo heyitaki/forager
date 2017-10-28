f610e4e30ec0ab71d0fa13afba0b201c685eac29
Michael Li <michael.li1324@gmail.com>
2017-10-27 21:10:38 -0700

    set up server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+app.post('/query', (req, res) => {
+  console.log(req.body);
 })
