003f2bb71926157163feffdad6da1be184bf2a04
Michael Li <michael.li1324@gmail.com>
2017-10-28 07:24:34 -0700

    search results page

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const url = require('url');
+const querystring = require('querystring');
 const bodyParser = require('body-parser');
+const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const pug = require('pug');
 const app = express();
 
-const compilePug = pug.compileFile('views/search-results.pug');
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, "views"))
-
-app.use('/static', express.static(path.join(__dirname, "public"));
-app.use(bodyParser.urlencoded({extended: true}));
 
 MongoClient.connect('mongodb://forage:forage@ds031741.mlab.com:31741/forage', (err, database) => {
   if (err) return console.log(err)
@@ -20,6 +19,20 @@ MongoClient.connect('mongodb://forage:forage@ds031741.mlab.com:31741/forage', (e
   })
 })
 
+app.get('/', (req, res) => {
+  var searchItem = req.query;
+  if(Object.keys(searchItem).length === 0) {
+    res.render('index');
+  } else {
+    res.render('searchResults', {search: searchItem.search});
+  }
+})
+
 app.post('/query', (req, res) => {
-  res.redirect( compilePug({name:'Michael'}));
+  res.redirect(url.format({
+    pathname: '/',
+    query: {
+      search: req.body.searchQuery
+    }
+  }))
 })
