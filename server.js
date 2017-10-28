e00d331903e4299c89b8766d3f69b6a3d094dc0b
Michael Li <michael.li1324@gmail.com>
2017-10-27 22:20:09 -0700

    set up database

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+MongoClient.connect('mongodb://forage:forage@ds031741.mlab.com:31741/forage', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, function() {
+    console.log('Listening on port 3000');
+  })
 })
 
 app.post('/query', (req, res) => {
-  console.log(req.body);
+  res.redirect('/');
 })
