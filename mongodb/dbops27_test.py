36df34570531de1c1a16b306f17e93e4f941faae
Akshath Sivaprasad <akshath@berkeley.edu>
2017-10-28 07:57:49 -0700

    adding support and testing tags

--- mongodb/dbops27_test.py
+++ mongodb/dbops27_test.py
@@ -1,12 +1,18 @@
 from dbops27 import dump, clear, search
 from bson.json_util import loads, dumps
+from dbadd_cs186_sp_2016_mt1 import add_exam as add1
+from dbadd_cs186_sp_2016_mt2 import add_exam as add2
 
 def test():
   #query = "  hello world  -y   2017  -a -c  midterm1    recursion"
   #print search(query)
-  query = "-t midterm1 hash"
+  query = "memory"
   result = search(query)
   for item in result:
     print dumps(item)
 
+# add1()
+# add2()
+# dump('questions')
+# dump('tags')
 test()
