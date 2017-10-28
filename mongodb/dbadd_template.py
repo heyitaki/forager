1badf162e9767260ab296d69be5176e8f85a05b4
Akshath Sivaprasad <akshath@berkeley.edu>
2017-10-28 07:58:36 -0700

    minor rename fix to accomadate tags db

--- mongodb/dbadd_template.py
+++ mongodb/dbadd_template.py
@@ -5,8 +5,8 @@ $ python dbops-test.py
 {u'category': u'innovate berkeley', u'term': u'fall', u'solution': u'Forage!', u'content': u'What is your name?', u'year': u'2017', u'_id': u'760541843', u'teacher': u'final'}
 '''
 
-from dbops27 import add, dump, clear
+from dbops27 import add_question, dump, clear
 
 # format: add(content, course, year, season, category, teacher, solution)
-add('What is your name?', 'cs61a', '2017', 'fall', 'final', 'denero', 'Forage!')
+add_question('What is your name?', 'cs61a', '2017', 'fall', 'final', 'denero', 'Forage!')
 dump()
