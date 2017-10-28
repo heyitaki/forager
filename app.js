e7335b3edf89d2f2ed5a869e48133f8f6c7bbd00
Michael Li <michael.li1324@gmail.com>
2017-10-28 11:57:07 -0700

    rest of template

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const pug = require('pug');
+const PythonShell = require('python-shell');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'pug');
 
-MongoClient.connect('mongodb://forage:forage@ds031741.mlab.com:31741/forage', (err, database) => {
+MongoClient.connect('mongodb://root:root@cluster0-shard-00-00-inppe.mongodb.net:27017,cluster0-shard-00-01-inppe.mongodb.net:27017,cluster0-shard-00-02-inppe.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(3000, function() {
@@ -23,8 +24,17 @@ app.get('/', (req, res) => {
   var searchItem = req.query;
   if(Object.keys(searchItem).length === 0) {
     res.render('index');
-  } else {
-    res.render('searchResults', {search: searchItem.search});
+  } else if(searchItem.isSearch) {
+    var options = {
+      mode: 'json',
+      args: ['memory'],
+      scriptPath: './mongodb',
+    }
+    PythonShell.run('search.py', options, function (err, results) {
+        if (err) throw err;
+        console.log(results[0]);
+        res.render('searchResults', {search: searchItem.search, results: results[0]});
+    });
   }
 })
 
@@ -32,7 +42,8 @@ app.post('/query', (req, res) => {
   res.redirect(url.format({
     pathname: '/',
     query: {
-      search: req.body.searchQuery
+      search: req.body.searchQuery,
+      isSearch: true
     }
   }))
 })
