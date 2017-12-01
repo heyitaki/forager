aefcb7071351802c9e17def1b641e547da217f58
Akshath Sivaprasad <akshath@berkeley.edu>
2017-11-30 20:46:25 -0800

    gcs/es upload & indexing works

--- elasticsearch/exam_data/upload.py
+++ elasticsearch/exam_data/upload.py
@@ -1,37 +1,45 @@
+from elasticsearch import Elasticsearch
+from google.cloud import storage
 import os
 import json
 
-def jsonify_search(q_img_path):
-  q_info_path = './question_info/' + replace_last(q_img_path, 'PNG', 'json')
-  s_img_path = './solutions/' + replace_last(q_img_path, 'q', 's')
-  exam_info_path = './exam_info/' + q_img_path[:-7] + '.json'
-  q_img_path = './questions/' + q_img_path
+def upload_to_elastic():
+  es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+  for filename in os.listdir('./image_question'):
+    [exam_info_path, question_info_path, question_image_path, solution_image_path] = get_paths(filename)
 
-  data = {}
+    upload_to_gcs(question_image_path.split('/')[-1], question_image_path)
+    upload_to_gcs(solution_image_path.split('/')[-1], solution_image_path)
+    print '%s uploaded successfully to GCS' % filename
 
-  with open(q_info_path) as q_info_file:
-    data.update(json.load(q_info_file))
+    data = {}
+    update_json(data, exam_info_path)
+    update_json(data, question_info_path)    
 
-  with open(exam_info_path) as exam_info_file:
-    data.update(json.load(exam_info_file))
+    es.indices.delete(index='exam', ignore=[400, 404])
+    es.index(index='exam', doc_type='question', body=json.dumps(data))
+    print 'JSON uploaded successfully to ES'
 
-  with open(q_img_path) as q_img_file:
-    q_img_data = q_img_file.read()
-    data['q_img'] = q_img_data.encode('base64')
+def upload_to_gcs(destination_blob_name, filepath):
+  storage_client = storage.Client.from_service_account_json('forager-upload.json')
+  bucket = storage_client.get_bucket('forager-qa-images')
+  blob = bucket.blob(destination_blob_name)
+  blob.upload_from_filename(filepath)
 
-  with open(s_img_path) as s_img_file:
-    s_img_data = s_img_file.read()
-    data['s_img'] = s_img_data.encode('base64')
-
-  print data
-  return json.dumps(data)
+##### HELPERS #####
+def get_paths(path):
+  exam_info_path = './info_exam/' + '_'.join(path.split('_')[:-1]) + '.json'
+  question_info_path = './info_question/' + replace_last(path, '.PNG', '.json')
+  question_image_path = './image_question/' + path
+  solution_image_path = './image_solution/' + replace_last(path, 'q', 's')
+  return [exam_info_path, question_info_path, question_image_path, solution_image_path]
 
 def replace_last(src_str, replace_what, replace_with):
   head, _sep, tail = src_str.rpartition(replace_what)
   return head + replace_with + tail
 
-def upload_to_elastic():
-  for filename in os.listdir('./questions'):
-    print jsonify_search(filename)
+def update_json(curr_json, path_to_json):
+  with open(path_to_json) as json_file:
+    curr_json.update(json.load(json_file))
 
 upload_to_elastic()
